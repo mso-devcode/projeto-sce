3747cc48a3165d365c8b5118533002150d27eb19
Carlos Gonzaga <mso.devcode@gmail.com>
2025-01-29 14:34:57 +0000

    Criado Classe Secretaria, como suas propriedades e metodos

--- src/main/java/ProjetoSCE.java
+++ src/main/java/ProjetoSCE.java
@@ -1,10 +1,28 @@
+
+import local.mso.models.Secretaria;
+
 /**
  *
  * @author NB-MSO-RJO
  */
 public class ProjetoSCE {
-
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        final String TITULO = "\t\t\tSISTEMA DE CONTROLE ESCOLAR";
+                
+        Secretaria secretaria = new Secretaria();
+        
+        carregarDadosSecretaria(secretaria);
+        
+        System.out.println(TITULO);
     }
+
+    private static void carregarDadosSecretaria(Secretaria secretaria) {
+        secretaria.setLocalizacao("Andar Terreo ao Lado da escada");
+        secretaria.setEmail("secretaria-sce@gmail.com");
+        secretaria.setId(01);
+        secretaria.setFuncioanamento("Horário\nmanhã - 08:00 h às 12:00 h\ntarde - 13:30 h às 16:00 h. ");
+    }
+
 }
---
+++ src/main/java/local/mso/models/Secretaria.java
@@ -0,0 +1,79 @@
+
+package local.mso.models;
+
+import java.util.Date;
+
+/**
+ *
+ * @author NB-MSO-RJO
+ */
+
+public class Secretaria {
+    
+      
+   /* ------------ VARIAVEIS DA CLASSE --------------------- */
+    private int id;
+    private String responsavel;
+    private String funcioanamento;
+    private String status;
+    private String email;
+    private String localizacao;
+    
+    public Secretaria(){};
+  
+   /* ------------ GETTER'S E SETTER'S --------------------- */
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getResponsavel() {
+        return responsavel;
+    }
+
+    public void setResponsavel(String responsavel) {
+        this.responsavel = responsavel;
+    }
+
+    public String getFuncioanamento() {
+        return funcioanamento;
+    }
+
+    public void setFuncioanamento(String funcioanamento) {
+        this.funcioanamento = funcioanamento;
+    }
+
+    
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLocalizacao() {
+        return localizacao;
+    }
+
+    public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
+    }
+    
+    
+   /* ------------ METODOS DA CLASSE --------------------- */
+    
+  
+}
